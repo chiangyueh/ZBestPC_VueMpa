671209f8dff577b48c9a4406571f7350ce867da5
yueh <y456123u@gmail.com>
2023-01-08 15:59:06 +0800

    copywebpackplugin拷貝文件

--- webpack.vue.config.js
+++ webpack.vue.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const { ProvidePlugin } = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: {
@@ -27,6 +28,14 @@ module.exports = {
       filename: "index.html",
       template: path.resolve(__dirname, "./src/index.html"),
       chunks: ["index"],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "./src/img"),
+          to: path.resolve(__dirname, "./dist/img"),
+        },
+      ],
     }),
   ],
 };
