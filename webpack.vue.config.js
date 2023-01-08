35fd314e146ba4035aa453c321f634a37232c395
yueh <y456123u@gmail.com>
2023-01-08 15:55:39 +0800

    html文件打包

--- webpack.vue.config.js
+++ webpack.vue.config.js
@@ -1,25 +1,32 @@
-const path = require('path')
-const {ProvidePlugin} = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const { ProvidePlugin } = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-    mode : 'development',
-    entry : './src/index.js',
-    output : {
-        path : path.resolve(__dirname,'./dist'),
-        filename : 'bundle.js'
-    },
-    module: {
-        rules : [
-            {
-                test : /\.css$/,
-                use : ['style-loader','css-loader']
-            }
-        ]
-    },
-    plugins : [
-        new ProvidePlugin({
-            $ : 'jquery',
-            jQuery : 'jquery'
-        }),
-    ]
-}
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: path.resolve(__dirname, "./src/index.html"),
+      chunks: ["index"],
+    }),
+  ],
+};
